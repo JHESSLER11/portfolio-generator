f0ba57cdff4c2a5d5070fee0083575cc4353f759
JHessler <jhessler11@gmail.com>
2022-02-20 10:15:32 -0500

    bug fixes

--- app.js
+++ app.js
@@ -132,20 +132,19 @@ const promptProject = portfolioData => {
       message: 'Would you like to enter another project?',
       default: false
     }
-    .then(promptProject)
-    .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
-      } else {
-        return portfolioData;
-      }
-    })
-  ]);
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-  console.log(projectAnswers)
+  console.log(portfolioData)
   });
